0b89fac2e0fa804e41f00d05aec2a8b9a894827e
armansw <stepanyan.arman982@gmail.com>
2023-10-07 14:45:49 -0600

    feat: added games pages

---
+++ src/games/district53/pages/Auctions.tsx
@@ -0,0 +1,23 @@
+export const Auctions = () => {
+    return (
+        <div>
+                    <div className="max-w-5xl mx-auto">
+                <div className=' py-12'>
+                <div className='grid grid-cols-4'>
+                  <div className="col-span-3">
+                  <div className="flex flex-row px-4">
+                  <div className='text-3xl text-primary font-bold'>Land Auctions</div>
+                  <div className="grow"></div>
+
+                  </div>
+                    <div className="px-4 py-16">
+                            <div className="bg-primary/10 bg-opacity-10 py-4 rounded text-center text-primary/60">
+                                There are no land auctions at the moment.
+                            </div>
+                    </div>
+                  </div>
+                </div>
+</div></div>
+        </div>
+    );
+}
